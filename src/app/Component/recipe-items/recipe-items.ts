eff863c59f04b853c12a61e3ea94501e68510a4b
Sengtri Bun <sengtri457@gmail.com>
2025-06-11 00:19:10 +0700

    Modify Sum

--- src/app/Component/recipe-items/recipe-items.ts
+++ src/app/Component/recipe-items/recipe-items.ts
@@ -1,11 +1,10 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { DataRecipeItems } from '../../models/Datarecipe.type';
 import { Recipe } from '../../services/recipe';
-import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-items',
-  imports: [DatePipe],
+  imports: [],
   templateUrl: './recipe-items.html',
   styleUrl: './recipe-items.css',
 })
@@ -13,10 +12,29 @@ export class RecipeItems implements OnInit {
   recipeService = inject(Recipe);
   displayProduct = signal<Array<DataRecipeItems>>([]);
   total = this.recipeService.total();
-  Address = 'Phnome Penh';
-  ManagerName = 'Bun Snegtri';
-  myDate: Date = new Date();
+  totalQty = this.recipeService.totalQty();
+  totalCash: number = this.recipeService.totalCash();
+  cash: number = this.recipeService.cash;
+  Address = ' street 123 Khan SenSok Phnome Penh';
+  Tell: string = '099706869';
   barcode: string = '1234567890';
+  Bankcard = Math.floor(Math.random() * parseInt(this.barcode));
+  // ApproveCode: any =
+  //   Math.floor(Math.random() * parseInt(this.barcode)) + 'äuaua';
+  getCustomCode(): string {
+    const numbers = this.barcode;
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let code = '#';
+    for (let i = 0; i < 3; i++) {
+      code += numbers[Math.floor(Math.random() * numbers.length)];
+      code += letters[Math.floor(Math.random() * letters.length)];
+    }
+    return code;
+  }
+  ApproveCode = this.getCustomCode();
+  constructor() {
+    console.log(this.Bankcard);
+  }
 
   ngOnInit(): void {
     this.displayProduct.set(this.recipeService.items);
--- src/app/models/Datarecipe.type.ts
+++ src/app/models/Datarecipe.type.ts
@@ -8,4 +8,5 @@ export type DataRecipeItems = {
   managerName?: string;
   date?: Date | string;
   address?: string;
+  star: string;
 };
