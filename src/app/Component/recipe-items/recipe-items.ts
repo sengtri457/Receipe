a3ee724f34b058840be738a85d1d90bf42103da0
Sengtri Bun <sengtri457@gmail.com>
2025-06-17 16:24:16 +0700

    modify Sum

--- src/app/Component/recipe-items/recipe-items.ts
+++ src/app/Component/recipe-items/recipe-items.ts
@@ -18,11 +18,14 @@ export class RecipeItems implements OnInit {
   Address = ' street 123 Khan SenSok Phnome Penh';
   Tell: string = '099706869';
   empt = '';
+  cashMoney = this.recipeService.cash;
   barcode: number = Math.floor(Math.random() * 10000000000);
   Bankcard = Math.floor(Math.random() * 1e9)
     .toString()
     .padStart(9, '0');
   formatted = this.Bankcard.replace(/(.{3})(?=\d)/g, '$1 ');
+  pricelist: Array<{ SellPrice: number }> = this.recipeService.items; // Assuming items is an array of objects with a price property
+  change: number = 0;
   getCustomCode(): string {
     const numbers: string = String(this.barcode);
     const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -37,8 +40,17 @@ export class RecipeItems implements OnInit {
   constructor() {
     console.log(this.Bankcard);
   }
-
+  CashTotal: number = 0;
+  ChangeCash: number = 0;
   ngOnInit(): void {
     this.displayProduct.set(this.recipeService.items);
+    if (this.cashMoney != 0) {
+      let sum = 0;
+      for (let i = 0; i < this.pricelist.length; i++) {
+        sum += this.pricelist[i].SellPrice;
+      }
+      this.CashTotal = sum;
+      this.ChangeCash = this.cashMoney - this.CashTotal;
+    }
   }
 }
